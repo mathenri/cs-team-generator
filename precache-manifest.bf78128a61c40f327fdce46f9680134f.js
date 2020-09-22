13e14bd0da85ffdf0fff590e2912fd2040986db8
Mattias Henriksson <henriksson.mj@gmail.com>
2020-09-22 18:54:30 +0200

    Updates

---
+++ precache-manifest.bf78128a61c40f327fdce46f9680134f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390ffcddd5d64fd56d791e0a0a7d5b6f",
+    "url": "/cs-team-generator/index.html"
+  },
+  {
+    "revision": "43d9333c59eeed4306ee",
+    "url": "/cs-team-generator/static/css/main.f2ed3644.chunk.css"
+  },
+  {
+    "revision": "8c180a25bf4f47e067a5",
+    "url": "/cs-team-generator/static/js/2.b0140600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cs-team-generator/static/js/2.b0140600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d9333c59eeed4306ee",
+    "url": "/cs-team-generator/static/js/main.13bbf2a0.chunk.js"
+  },
+  {
+    "revision": "baffc4922b9cbfc459a1",
+    "url": "/cs-team-generator/static/js/runtime-main.034a0a57.js"
+  },
+  {
+    "revision": "f6060291e5a81bd6623d48ab2f2da2e8",
+    "url": "/cs-team-generator/static/media/lan.f6060291.jpeg"
+  }
+]);
